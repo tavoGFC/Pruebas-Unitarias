e19db709cca19ae666729e1fed2927b50faf16e1
Randy Martínez <randyma01@gmail.com>
2018-03-23 09:37:28 -0600

    Código en C# + NUnit. Sólo las clases de la prueba

---
+++ Example NUnit/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploNUnit
+{
+   
+    class Cliente
+    {
+        public string nombreCompleto { get; set; }
+        public int edad { get; set; }
+        public string tipoCliente { get; set; } //Clientes: estudiantes (15%) o regulares (0%).
+    }
+}
---
+++ Example NUnit/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploNUnit
+{
+    class Ticket
+    {
+        public string nombrePelicula { set; get;}
+        public bool estreno { set; get; }
+        public int costo { set; get; }
+    }
+}
